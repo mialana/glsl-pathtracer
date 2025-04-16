bd736648e11cae9c3da8cd114f0b582fd2149cd7
mialana <96770662+mialana@users.noreply.github.com>
2025-04-16 01:21:36 -0400

    Create fresnel test scene

--- src/mygl.cpp
+++ src/mygl.cpp
@@ -93,7 +93,7 @@ void MyGL::initializeGL()
 
     m_environmentCubemapFB.create(true);
 
-    this->loadJSON(":/jsons/PT_glassBallBox.json");
+    this->loadJSON(":/jsons/testScene.json");
 
     m_timer.start(16);
 }
